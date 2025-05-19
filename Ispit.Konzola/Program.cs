0d2c97a4071c9bf03639626b7e057db971a1e5e1
Ivan Vidović <ivodkw@gmail.com>
2025-05-19 19:08:25 +0200

    Add project files.

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,67 @@
+﻿using Ispit.Model.Modeli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Konzola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KreirajTriUcenika();
+        }
+
+        private static void KreirajTriUcenika()
+        {
+            int brojac = 1;
+            var listaUcenika = new List<Ucenik>();
+
+            Console.WriteLine("Molimo da se kreiraju tri učenika.");
+            while (brojac <= 3)
+            {
+                Console.WriteLine($"Podaci učenika {brojac}:");
+                Console.WriteLine("Ime:");
+                string ime = Console.ReadLine();
+                Console.WriteLine("Prezime:");
+                string prezime = Console.ReadLine();
+                Console.WriteLine("Datum rođenja:");
+                DateTime datumRodjenja;
+                if (!DateTime.TryParse(Console.ReadLine(), out datumRodjenja))
+                {
+                    Console.WriteLine("Neispravni datum rodjenja, zapisujemo aktualni datum");
+                    datumRodjenja = DateTime.Now;
+                }
+                Console.WriteLine("Prosjek: (koristiti decimalni zarez)");
+                double prosjek;
+
+                if (!double.TryParse(Console.ReadLine(), out prosjek))
+                {
+                    Console.WriteLine("Neispravni broj za prosjek, zapisujemo 1");
+                    prosjek = 1.00;
+                }
+
+                var ucenik = new Ucenik(ime, prezime, datumRodjenja, prosjek);
+
+                listaUcenika.Add(ucenik);
+                brojac++;
+
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Svi podaci unesenih ucenika:");
+            
+            foreach (var ucenik in listaUcenika)
+            {
+                Console.WriteLine($"{ucenik.Ime} {ucenik.Prezime}, datum rođenja: {ucenik.DatumRodjenja:d}, " +
+                    $"prosjek: {ucenik.Prosjek:F2}, ocjena: {ucenik.IspisProsjeka()}, starost: {ucenik.Starost()}");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Ispit.Model/Modeli/Ucenik.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model.Modeli
+{
+    public class Ucenik
+    {
+        #region Svojstva
+
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public double Prosjek { get; set; }
+
+        #endregion
+
+        #region Metode
+
+        public Ucenik(string ime, string prezime, DateTime datumRodjenja, double prosjek)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            DatumRodjenja = datumRodjenja;
+            Prosjek = prosjek;
+        }
+        /// <summary>
+        /// Vraća zaokruženu starost učenika u godinama
+        /// </summary>
+        /// <returns></returns>
+        public int Starost()
+        {
+            return DateTime.Now.Year - DatumRodjenja.Year;
+        }
+        /// <summary>
+        /// Ispisuje prosjek u opisnom formatu
+        /// </summary>
+        /// <returns></returns>
+        public string IspisProsjeka()
+        {
+            string opisnaOcjena;
+            switch (Prosjek)
+            {
+                case double p when (p >= 1 && p <= 1.49):
+                    opisnaOcjena =  "nedovoljan";
+                    break;
+                case double p when (p >= 1.50 && p <= 2.49):
+                    opisnaOcjena =  "dovoljan";
+                    break;
+                case double p when (p >= 2.50 && p <= 3.49):
+                    opisnaOcjena = "dobar";
+                    break;
+                case double p when (p >= 3.50 && p <= 4.49):
+                    opisnaOcjena = "vrlo dobar";
+                    break;
+                case double p when (p >= 4.50 && p <= 5):
+                    opisnaOcjena = "odličan";
+                    break;
+                default:
+                    opisnaOcjena = "nije ispravna ocjena";
+                    break;
+            }
+
+            return opisnaOcjena; 
+        }
+
+
+        #endregion
+    }
+}
